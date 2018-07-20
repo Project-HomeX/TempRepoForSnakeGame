49c1a71842c1a4974887c303a3a86f1818a0a27a
Natenael Kelkay <natikelkay@gmail.com>
2018-07-20 12:40:08 -0400

    Added Boundaries | If the snake touches a wall it goes to the other side of the screen

--- src/Snake.java
+++ src/Snake.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 /**
@@ -11,27 +13,51 @@ public class Snake {
 	float yspeed = 0;
 	float scl = 20;
 	PApplet parrent;
+	
+	ArrayList<Integer> snakeXLength = new ArrayList<Integer>();
+	ArrayList<Integer> snakeYLength = new ArrayList<Integer>();
+	private int lengthOfSnake = 3;
+	
+	
 	public Snake(PApplet p, float scl) {
-		// TODO Auto-generated constructor stub
 		this.scl = scl;
 		parrent = p;
 		this.x = p.width/2;
 		this.y = p.height/2;
 	}
-	public void update(){
-		x = x+ xspeed;
-		y = y+yspeed;
-	}
+	
+	//Getter and setters
 	public float getX(){
 		return x;
 	}
 	public float getY(){
 		return y;
 	}
+	
+	public void update(){
+		if( x >= 590) {
+			x = 0;
+		}
+		else if (x <= 10) {
+			x = 600;
+		}
+		
+		if( y >= 590) {
+			y = 0;
+		}
+		else if (y <= 10) {
+			y = 600;
+		}
+		
+		x = x+ xspeed;
+		y = y+yspeed;
+	}
+	
 	public void show(){
 		parrent.fill(255);
 		parrent.rect(x,y,scl,scl);
 	}
+	
 	public void dir(int x, int y){
 		xspeed = x;
 		yspeed = y;
